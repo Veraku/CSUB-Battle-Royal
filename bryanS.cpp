2b41866d04baf702b76aa811b4dce3c6a6cd272f
Bryan <Bryan@ksiwidgets.com>
2018-09-25 13:28:57 -0700

    Added a comment

--- bryanS.cpp
+++ bryanS.cpp
@@ -1,10 +1,13 @@
+
+// bs 2018-09-25: External file for Final Project CMPS 3350
+
 #include <iostream>
 #include "fonts.h"
-void bryan_credits(int x, int y){
+
+void bryan_credits(int x, int y)
+{
 	Rect r;
-  r.bot = y;
 	r.left = x;
+	r.bot = y;
 	ggprint8b(&r, 16, 0x00ff0000, "Bryan Sanders");
-
-
 }
